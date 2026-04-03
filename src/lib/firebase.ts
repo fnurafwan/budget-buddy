40b2974d749fda77cbec9400848e3409f32cb237
fnurafwan <fnurafwan@gmail.com>
2026-04-03 20:07:43 +0700

    ganti koneksi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBC99uNqFGwaDivfgo369Ql9aXCQ2SMiLs",
-  authDomain: "budget-buddy-92c32.firebaseapp.com",
-  projectId: "budget-buddy-92c32",
-  storageBucket: "budget-buddy-92c32.firebasestorage.app",
-  messagingSenderId: "462678414273",
-  appId: "1:462678414273:web:5fcefffc093202ae431540",
-  measurementId: "G-J4Q09NGR5C"
+  apiKey: "AIzaSyAZOdgb9v08Wdx-rquE3GzA0xeC1SV_Auw",
+  authDomain: "budget-buddy-673f1.firebaseapp.com",
+  projectId: "budget-buddy-673f1",
+  storageBucket: "budget-buddy-673f1.firebasestorage.app",
+  messagingSenderId: "293715708191",
+  appId: "1:293715708191:web:2fab5a188b6b592761882a",
+  measurementId: "G-133J6MJ5B4"
 };
 
 // Singleton pattern agar tidak initialize berkali-kali saat hot-reload
--- src/lib/firebase_lm.ts
+++ src/lib/firebase_lm.ts
@@ -10,13 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ_lq8OQxVFu9tZgY0txs37V6jTkoqrck",
-  authDomain: "pereleman-9202b.firebaseapp.com",
-  projectId: "pereleman-9202b",
-  storageBucket: "pereleman-9202b.firebasestorage.app",
-  messagingSenderId: "586809740737",
-  appId: "1:586809740737:web:fcaf9dd29f55321b4bb340",
-  measurementId: "G-6KMYB3MRS1"
+  apiKey: "AIzaSyDwRrHIsXNPdLPe0J2zpMbpmNHiJssa67o",
+  authDomain: "pereleman-ff655.firebaseapp.com",
+  projectId: "pereleman-ff655",
+  storageBucket: "pereleman-ff655.firebasestorage.app",
+  messagingSenderId: "1057218216127",
+  appId: "1:1057218216127:web:2751735a43db359927460a"
 };
 
 // Initialize Firebase
