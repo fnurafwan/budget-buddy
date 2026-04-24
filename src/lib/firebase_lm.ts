da4fd03b9b8324ca735152c6fbcccf7b2b1e8df3
fnurafwan <fnurafwan@gmail.com>
2026-04-24 13:41:09 +0700

    Update firebase_lm.ts

--- src/lib/firebase_lm.ts
+++ src/lib/firebase_lm.ts
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwRrHIsXNPdLPe0J2zpMbpmNHiJssa67o",
-  authDomain: "pereleman-ff655.firebaseapp.com",
-  projectId: "pereleman-ff655",
-  storageBucket: "pereleman-ff655.firebasestorage.app",
-  messagingSenderId: "1057218216127",
-  appId: "1:1057218216127:web:2751735a43db359927460a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
