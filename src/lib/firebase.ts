feab7d8613b66b5d27dc2eb4cdb1d4310838aabd
fnurafwan <fnurafwan@gmail.com>
2026-04-24 13:40:25 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZOdgb9v08Wdx-rquE3GzA0xeC1SV_Auw",
-  authDomain: "budget-buddy-673f1.firebaseapp.com",
-  projectId: "budget-buddy-673f1",
-  storageBucket: "budget-buddy-673f1.firebasestorage.app",
-  messagingSenderId: "293715708191",
-  appId: "1:293715708191:web:2fab5a188b6b592761882a",
-  measurementId: "G-133J6MJ5B4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Singleton pattern agar tidak initialize berkali-kali saat hot-reload
